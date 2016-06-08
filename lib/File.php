ab8314bab4367e605707ca47382d7a4b70099b3c
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2016-06-08 10:55:59 +0900

    完成風味

---
+++ lib/File.php
@@ -0,0 +1,32 @@
+<?php
+
+require('FizzBuzz.php');
+
+class File
+{
+  public static function write($file) {
+    $str = '';
+    for ($i = 0; $i < sizeof(FizzBuzz::array); $i++) {
+      if($i % 2 === 0) {
+        $str = $str."FizzBuzz::array[$i]";
+        $str = $str.",";
+      }
+      if($i % 2 === 1) {
+        $str = $str."FizzBuzz::array[$i]";
+      }
+    }
+    return fwrite($file, $str);
+  }
+
+  public static function read($file) {
+    $data = fread($file, 255);
+    $datas = split(',', $data);
+    for($i = 0; $i < sizeof($datas); $i++) {
+      if($i === 0) {
+        echo "datas[$i]";
+      } else {
+        echo " : datas[$i]";
+      }
+    }
+  }
+}
--- lib/FizzBuzz.php
+++ lib/FizzBuzz.php
@@ -1,35 +1,37 @@
 <?php
 
+require('File.php');
+
 class FizzBuzz
 {
   public static $array = [];
 
   public static function start()
   {
-    while (true) {
+    while(true) {
       print "好きなモードをえらんでねはぁと\n";
       print "モードがわからんとか言うな\n";
       $x = trim(fgets(STDIN));
-      if ($x === '1') {
+      if($x === '1') {
         print "FizzBuzzなんだから整数を入力しなさいよね！！\n";
         $x2 = trim(fgets(STDIN));
-        if (checkFizzBuzz($x)) {
-          if ($x2 === '0') {
+        if(checkFizzBuzz($x)) {
+          if($x2 === '0') {
             print "$x2\n";
             $array[] = $x2;
             $array[] = "$x2\n";
           }
-          else if ($x2 % 15 === 0) {
+          else if($x2 % 15 === 0) {
             print "FizzBuzz\n";
             $array[] = $x2;
             $array[] = "FizzBuzz\n";
           }
-          else if ($x2 % 5 === 0) {
+          else if($x2 % 5 === 0) {
             $array[] = $x2;
             print "Buzz\n";
             $array[] = "Buzz\n";
           }
-          else if ($x2 % 3 === 0) {
+          else if($x2 % 3 === 0) {
             $array[] = $x2;
             $array[] = "Fizz\n";
             print "Fizz\n";
@@ -41,17 +43,27 @@ class FizzBuzz
           }
         }
       }
-      else if ($x === '2') {
-        for ($i = 0; $i < sizeof($array); $i += 2) {
+      else if($x === '2') {
+        for($i = 0; $i < sizeof($array); $i += 2) {
           $j = $i + 1;
-          print "$array[$i] : $array[$j]";
+          print "$array[$i] ";
+          print ":";
+          print "$array[$j]";
         }
+      } else if($x === '3') { // 履歴保存
+        $file = fopen('data.txt', 'w');
+        File::write($file); // arrayの中身をファイルに書き出す
+        fclose($file);
+      } else if($x === '4') { // 履歴出力
+        $file = fopen('data.txt', 'w+');
+        File::read($file); // ファイル出力用関数
+        fclose($file);
       }
-      else if ($x === '0') {
+      else if($x === '0') {
          break;
       }
       else {
-        print "そんなモードないんだからね！！\n";
+        echo "そんなモードないんだからね！！\n";
       } //end if
     }
     return 0; //end fizz buzz
